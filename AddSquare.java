e5a1bdebfa7e9f4fdbae871a7b2fa7d239559e19
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-06-10 08:39:56 -0400

    that was painful

--- AddSquare.java
+++ AddSquare.java
@@ -2,32 +2,39 @@ import processing.core.*;
 import java.awt.Color;
 
 public class AddSquare extends PApplet {
-    PImage img;
+    PImage img, out;
 
     public void setup() {
-        img = loadImage("./QR.png");
-        img.resize(500, 500);
+      //size(500, 500);
+      img = loadImage("/home/yicheng-wang/github/src/QR.png");
+      img.resize(500, 500);
+      out = transform(img);
+      out.save("./QR_supermod.png");
     }
 
     public void draw() {
-        transform(img);
-        //image(out, 0, 0);
-        //exit();
+      //image(out, 0, 0);
+      //exit();
     }
 
-    public void transform(PImage img) {
-        //applet.size(img.width, img.height);
-        noStroke();
-        image(img, 0, 0);
-        fill(000, 75);
-        float x = 98;
-        float codeSize = 450;
-        float boxSize = 16f / 450 * codeSize;
-        float offset = 18f / 450 * codeSize;
-        rect(x, x, boxSize, boxSize);
-        rect(codeSize - x + offset, x, boxSize, boxSize);
-        rect(x, codeSize - x, boxSize, boxSize);
+    public PImage transform(PImage img) {
+      //applet.size(img.width, img.height);
+      PGraphics g = createGraphics(500, 500);
+      g.beginDraw();
+      g.noStroke();
+      g.image(img, 0, 0);
+      g.fill(000, 75);
+      float x = 98;
+      float codeSize = 450;
+      float boxSize = 16f / 450 * codeSize;
+      float offset = 18f / 450 * codeSize;
+      g.rect(x, x, boxSize, boxSize);
+      g.rect(codeSize - x + offset, x, boxSize, boxSize);
+      g.rect(x, codeSize - x, boxSize, boxSize);
+      g.endDraw();
+      return g;
     }
+
 
     public static void main(String args[]) {
         PApplet.main(new String[] { "--present", "AddSquare" });
