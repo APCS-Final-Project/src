f959b7b12e96a71347cd0f8c5c108063ca5c8e97
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-06-10 11:55:48 -0400

    removed useless files

--- GenerateQRCode.java
+++ GenerateQRCode.java
@@ -1,3 +1,5 @@
+import processing.core.*;
+import java.util.Scanner;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -13,26 +15,39 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
- 
-public class GenerateQRCode {
- 
-    /**
-     * @param args
-     * @throws WriterException
-     * @throws IOException
-     */
-    public static void main(String[] args) throws WriterException, IOException {
-        String qrCodeText = "hi jake";
-        String filePath = "QR.png";
-        int size = 1000;
-        String fileType = "png";
-        File qrFile = new File(filePath);
-        createQRImage(qrFile, qrCodeText, size, fileType);
-        System.out.println("DONE");
+
+public class GenerateQRCode extends PApplet {
+    static PImage img, out;
+    static String text;
+
+    public void setup() {
+      //size(500, 500);
+      //img = loadImage("/home/yicheng-wang/github/src/QR.png");
+      img.resize(500, 500);
+      out = transform(img);
+      out.save("./QR_supermod.png");
+      exit();
     }
- 
-    private static void createQRImage(File qrFile, String qrCodeText, int size,
-            String fileType) throws WriterException, IOException {
+
+    public PImage transform(PImage img) {
+      //applet.size(img.width, img.height);
+      PGraphics g = createGraphics(500, 500);
+      g.beginDraw();
+      g.noStroke();
+      g.image(img, 0, 0);
+      g.fill(000, 75);
+      float x = 98;
+      float codeSize = 450;
+      float boxSize = 16f / 450 * codeSize;
+      float offset = 18f / 450 * codeSize;
+      g.rect(x, x, boxSize, boxSize);
+      g.rect(codeSize - x + offset, x, boxSize, boxSize);
+      g.rect(x, codeSize - x, boxSize, boxSize);
+      g.endDraw();
+      return g;
+    }
+
+    private static PImage createQRImage(String qrCodeText, int size) throws WriterException {
         // Create the ByteMatrix for the QR-Code that encodes the given String
         Hashtable<EncodeHintType,Object> hintMap = new Hashtable<EncodeHintType,Object>();
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
@@ -59,7 +74,23 @@ public class GenerateQRCode {
                 }
             }
         }
-        ImageIO.write(image, fileType, qrFile);
+
+        // Convert to PImage -- yeech
+        PImage out = new PImage(image.getWidth(), image.getHeight(), PConstants.ARGB);
+        image.getRGB(0, 0, out.width, out.height, out.pixels, 0, out.width);
+        out.updatePixels();
+        return out;
     }
- 
+
+
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter what you want to encode please: ");
+        text = sc.nextLine();
+        try {
+            img = createQRImage(text, 500);
+        } catch (WriterException e) {}
+        PApplet.main(new String[] { "--present", "AddSquare" });
+    } 
 }
